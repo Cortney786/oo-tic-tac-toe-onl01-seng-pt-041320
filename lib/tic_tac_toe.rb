5c37da2e0d4082459b1bdf4ec745015a0637118a
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-26 15:28:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,33 +6,33 @@ class TicTacToe
   end  
 end
   
-#   WIN_COMBINATIONS = [
-#     [0,1,2], 
-#     [3,4,5], 
-#     [6,7,8], 
-#     [0,3,6], 
-#     [1,4,7],
-#     [2,5,8], 
-#     [0,4,8], 
-#     [6,4,2]  
-#   ] 
-#   end 
-# end
+  WIN_COMBINATIONS = [
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    [0,3,6], 
+    [1,4,7],
+    [2,5,8], 
+    [0,4,8], 
+    [6,4,2]  
+  ] 
+  end 
+end
 
   
-#     def display_board
-#       puts "#{boards[0]} | #{boards[1]} | #{boards[2]}" 
-#       puts "-----------"
-#       puts "#{boards[3]} | #{boards[4]} | #{boards[5]}"
-#       puts "-----------"
-#       puts "#{boards[6]} | #{boards[7]} | #{boards[8]}"
-#   end 
+    def display_board
+      puts "#{boards[0]} | #{boards[1]} | #{boards[2]}" 
+      puts "-----------"
+      puts "#{boards[3]} | #{boards[4]} | #{boards[5]}"
+      puts "-----------"
+      puts "#{boards[6]} | #{boards[7]} | #{boards[8]}"
+  end 
  
-# def input_to_index(input) 
-#   input.to_i - 1
-#   end 
+def input_to_index(input) 
+  input.to_i - 1
+  end 
   
-#   def move(index, player = "X")
-#     @board[index] = player
-#   end 
+  def move(index, player = "X")
+    @board[index] = player
+  end 
  
