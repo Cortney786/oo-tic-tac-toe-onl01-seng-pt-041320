558d7ee4f227178d1307bbe54f2ee668efbb9fc1
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-26 16:04:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(location, index, current_player)
+def move(location, index = current_player)
   @board[location.to_i - 1] = current_player
 end
 
